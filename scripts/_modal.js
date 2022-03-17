4b761884eda92dd0e893dfd4fc9aa8fc7a25ec6b
JC <39320835+mechcar@users.noreply.github.com>
2022-03-17 12:00:18 -0400

    2022-03-17

---
+++ scripts/_modal.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-modal");
+const closeModal = document.querySelector(".close-modal");
+
+openModal.addEventListener("click", () => {
+	modal.showModal();
+});
